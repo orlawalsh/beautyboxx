a22eaa715ac6d667f94e41c13bd8b3d82f80403a
Orla Walsh <orlawalsh@outlook.com>
2017-03-15 00:30:28 +0000

    Input Form added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,113 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+    import api from './test/stubAPI';
+    import  './App.css';
+    import buttons from './config/buttonsConfig';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+var ProductForm = React.createClass({
+        getInitialState: function() {
+           return { name: '', price: '', review: '', rating: ''};
+       },
+       handleNameChange: function(e) {
+            this.setState({name: e.target.value});
+       },
+       handlePriceChange: function(e) {
+           this.setState({price: e.target.value});
+       },
+       handleReviewChange: function(e) {
+           this.setState({review: e.target.value});
+       },
+       handleRatingChange: function(e) {
+           this.setState({rating: e.target.value});
+       },
+       handleSubmit: function(e) {
+        e.preventDefault();
+        var name = this.state.name.trim();
+        var price = this.state.price.trim();
+        var review = this.state.review.trim();
+        var rating = this.state.rating.trim();
+        if (!name || !price || !review || !rating) {
+          return;
+        }
+        this.props.addHandler(name,price,review,rating);
+        this.setState({name: '', price: '', review: '', rating: ''});
+        this.setState({status: ''});
+       },  
+       render: function(){
+          return (
+            
+            <tr>
+              <td>
+              <input type="text" className="form-control" 
+                     placeholder="Name"
+                     value={this.state.name}
+                     onChange={this.handleNameChange} />
+              </td>
+              <td>
+              <input min="0.01" type="number" className="form-control"
+                     placeholder="Price"
+                     value={this.state.price}
+                     onChange={this.handlePriceChange} />
+              </td>
+              <td>
+              <input type="textarea" className="form-control" 
+                     placeholder="Review"
+                     value={this.state.review}
+                     onChange={this.handleReviewChange} />
+              </td>
+                 <td>
+              <input type="number" min="1" max="5" className="form-control" 
+                     placeholder="Rating"
+                     value={this.state.rating}
+                     onChange={this.handleRatingChange} />
+              
+              </td>
+              <td>
+              <input type="button" className="btn btn-primary" value="Submit Review"
+                       onClick={this.handleSubmit} />
+              </td>
+              <td>
+              </td>
+            </tr>
+           
+            )
+        }
+      });
 
-export default App;
+   var ProductApp = React.createClass({
+
+          getInitialState: function() {
+           return { search: '', sort: 'name' } ;
+         }, 
+          
+          deleteProduct : function(k) {
+             api.delete(k);
+             this.setState( {} ) ;
+          },
+          addProduct : function(n,p,r,a) {
+             api.add(n,p,r,a) ;
+             this.setState({});
+          },
+          updateProduct : function(key,n,p,r,a) {
+              if (api.update(key,n,p,r,a) )  { 
+                  this.setState({});  
+              }             
+          },  
+
+          render: function(){
+                    var products = api.getAll();
+
+                    <div>
+                       <div className="logo"></div>
+                    <ProductsTable products={filteredList} 
+                          deleteHandler={this.deleteProduct}
+                          addHandler={this.addProduct} 
+                          updateHandler={this.updateProduct}  />
+                       
+                    </div>
+
+                    );
+              }
+
+          });
+
+export default ProductApp;
